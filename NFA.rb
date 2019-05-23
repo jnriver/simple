f5a42f309f44d756a5316a19f284c3e62bb4942b
Jiang Nan <jiangnan@hc360.com>
2019-05-23 11:59:57 +0800

    Add free move for NFA

--- NFA.rb
+++ NFA.rb
@@ -22,6 +22,14 @@ class NFARulebook < Struct.new(:rules)
   def rules_for(state, character)
     rules.select {|rule| rule.applies_to?(state, character)}
   end
+  def follow_free_moves(states)
+    more_states = next_states(states, nil)
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states + more_states)
+    end
+  end
 end
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
@@ -36,10 +44,13 @@ class NFA < Struct.new(:current_states, :accept_states, :rulebook)
       read_character(char)
     end
   end
+  def current_states
+    rulebook.follow_free_moves(super)
+  end
 end
 
 class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
-  def accept?(string)
+  def accepts?(string)
     to_nfa.tap {|nfa| nfa.read_string(string)}.accepting?
   end
   def to_nfa
@@ -48,11 +59,14 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
 end
 
 rulebook = NFARulebook.new([
-  FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
-  FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
-  FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+  FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+  FARule.new(2, 'a', 3), FARule.new(3, 'a', 2),
+  FARule.new(4, 'a', 5), FARule.new(5, 'a', 6),
+  FARule.new(6, 'a', 4)
 ])
 
-nfa_design = NFADesign.new(1, [4], rulebook)
+nfa_design = NFADesign.new(1, [2, 4], rulebook)
 
-puts nfa_design.accept?('bbbbb')
+puts nfa_design.accepts?('aa')
+puts nfa_design.accepts?('aaa')
+puts nfa_design.accepts?('aaaaa')
